8333ed615e787b09482e153d745c2c463756426c
Sarah-Lewis <63777835+Sarah-Lewis@users.noreply.github.com>
2020-06-11 17:41:00 +0100

    update

--- JavaScript/java.js
+++ JavaScript/java.js
@@ -14,3 +14,13 @@ $(document).ready(function(){
     });
   });
 });
+
+// GP LIST >
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
