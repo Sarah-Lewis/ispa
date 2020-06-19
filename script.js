c97ebe62b7bd132943cb6ad5dd9b6a236bd2c04a
Sarah-Lewis <63777835+Sarah-Lewis@users.noreply.github.com>
2020-06-19 14:08:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-// ISPA cheat sheet >
+// Copy function:
+
+var button = document.getElementById("copyID"),
+input = document.getElementById("content");
+
+button.addEventListener("click", function(event) {
+event.preventDefault();
+input.select();
+document.execCommand("copy");
+});
+
+// GP list search function:
 
 function myFunction() {
-  var copyText = document.getElementById("myInput");
-  copyText.select();
-  copyText.setSelectionRange(0, 99999)
-  document.execCommand("copy");
+var input, filter, table, tr, td, i, txtValue;
+input = document.getElementById("myInput");
+filter = input.value.toUpperCase();
+table = document.getElementById("myTable");
+tr = table.getElementsByTagName("tr");
+for (i = 0; i < tr.length; i++) {
+td = tr[i].getElementsByTagName("td")[0];
+if (td) {
+  txtValue = td.textContent || td.innerText;
+if (txtValue.toUpperCase().indexOf(filter) > -1) {
+  tr[i].style.display = "";
+} else {
+  tr[i].style.display = "none";
 }
-
-
-// GP LIST >
-
-function myFunction() {
-  var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }       
-  }
+}       
 }
+};
